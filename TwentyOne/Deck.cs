399e3ba10db3490e08ad96455c098df7b7fd1a4a
Allan Reitan <careitan2000@gmail.com>
2019-04-22 11:15:59 -0700

    Moved Shuffle into Deck class
    Moved Shuffle into deck class

--- TwentyOne/Deck.cs
+++ TwentyOne/Deck.cs
@@ -41,5 +41,25 @@ namespace TwentyOne
             }
         }
         public List<Card> Cards { get; set; }
+
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
+        {
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
+                timesShuffled++;
+            }
+            return deck;
+        }
     }
 }
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -12,7 +12,7 @@ namespace TwentyOne
         {
             Deck deck = new Deck();
             int timesShuffled = 0;
-            deck = Shuffle(deck, out timesShuffled, 3);
+            deck = Deck.Shuffle(deck, out timesShuffled, 3);
 
             foreach (Card card in deck.Cards)
             {
@@ -23,33 +23,5 @@ namespace TwentyOne
             Console.ReadLine();
         }
 
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                List<Card> TempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    TempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = TempList;
-                timesShuffled++;
-            }
-            return deck;
-        }
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
