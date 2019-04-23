f7cc5ac44cdb762df98d950ae69fa0e4a7539426
Allan Reitan <careitan2000@gmail.com>
2019-04-23 12:45:33 -0700

    Dealer Class
    Dealer Class

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,17 +10,22 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck = Deck.Shuffle(deck, out timesShuffled, 3);
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times shuffled {0}", timesShuffled);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            game.ListPlayers();
             Console.ReadLine();
+
+            //Deck deck = new Deck();
+            //int timesShuffled = 0;
+            //deck = Deck.Shuffle(deck, out timesShuffled, 3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.WriteLine("Times shuffled {0}", timesShuffled);
+            //Console.ReadLine();
         }
 
     }
